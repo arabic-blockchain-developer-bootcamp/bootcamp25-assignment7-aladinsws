1b86e26ed4c1abc38aec6ac107069d167a324417
aladinsws <aladinsws@gmail.com>
2025-03-10 20:47:10 +0100

    create TND ERC20 token

--- src/Assignment7.sol
+++ src/Assignment7.sol
@@ -2,17 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Assignment7 is ERC20 {
+
+contract Assignment7 is ERC20 , Ownable {
     // Constructor to initialize the token
     // Fill in the parameters for the token name and symbol
-    constructor() ERC20() {}
+    constructor() ERC20("Dinar","TNDT") Ownable(msg.sender) {}
 
     // Function to mint tokens
     // Fill in the visibility modifier to be external
     // Set params as to address and amount
-    function mint() {
-        // Fill in the logic
-        // call _mint function with to and amount params
+    function mint(address _to,uint256 _amount) external onlyOwner {
+        _mint(_to,_amount);
     }
 }
